d8be0b0ae3f0954e4859cba8b4ed818213c16ad1
Student <cecylbahati@gmail.com>
2025-03-26 21:33:53 +0300

    files

--- emailValidator.test.js
+++ emailValidator.test.js
@@ -1,7 +1,7 @@
 const isValidEmail = require('./emailValidator');
 
-test('validates "test@example.com" as a valid email', () => {
-  expect(isValidEmail('test@example.com')).toBe(true);
+test('validates "cecylbahati@gmail.com" as a valid email', () => {
+  expect(isValidEmail('cecylbahati@gmail.com')).toBe(true);
 });
 
 test('validates "invalid-email" as an invalid email', () => {
